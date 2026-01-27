031476af0cdcd3446f99cd64199686ef77761572
pob_git <sahapob.gg@gmail.com>
2026-01-27 18:58:08 +0700

    added loadstring.lua, funtion setup saber && sharkman and add multi buy melee ( select )

---
+++ loadstring.lua
@@ -0,0 +1,10 @@
+getgenv().Configs = {
+    ["Melee"] = {
+        ["Enable"] = false, -- เซ็ทค่าตอนแรกได้ว่าจะเปิดใช้งานฟังชั่นเลยไหม จะได้ไม่ต้องกดใน Ui
+        ["Select"] = nil, -- "Sanguine Art", "Dragon Talon", "Sharkman Karate"
+    },
+    ["Setup"]= {
+        ["Enable"] = false, -- เซ็ทค่าตอนแรกได้ว่าจะเปิดใช้งานฟังชั่นเลยไหม จะได้ไม่ต้องกดใน Ui
+    }
+}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Gl1tchl-4r/buy-melee/refs/heads/main/script.lua'))()
--- script.lua
+++ script.lua
@@ -1,4 +1,12 @@
--- loadstring(game:HttpGet('https://raw.githubusercontent.com/Gl1tchl-4r/buy-melee/refs/heads/main/script.lua'))()
+-- getgenv().Configs = {
+--     ["Melee"] = {
+--         ["Enable"] = true, -- เซ็ทค่าตอนแรกได้ว่าจะเปิดใช้งานฟังชั่นเลยไหม จะได้ไม่ต้องกดใน Ui
+--         ["Select"] = "Dragon Talon", -- "Sanguine Art", "Dragon Talon", "Sharkman Karate"
+--     },
+--     ["Setup"]= {
+--         ["Enable"] = false, -- เซ็ทค่าตอนแรกได้ว่าจะเปิดใช้งานฟังชั่นเลยไหม จะได้ไม่ต้องกดใน Ui
+--     }
+-- }
 
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer and game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)") or game:GetService("Players").LocalPlayer.Character
 
@@ -53,11 +61,6 @@ local function creatTweenPart()
     return part
 end
 
--- local function tp(pos)
---     isTweening = false
---     hrp.CFrame = pos
--- end
-
 local function tween(pos)
     local TweenPart = game.Workspace:FindFirstChild("TweenPart")
     if not TweenPart then TweenPart = creatTweenPart() end
@@ -77,23 +80,91 @@ local function tween(pos)
     isTweening = false
 end
 
-local function main()
+getgenv().meleeConfigs = {
+    ["Position"] = nil,
+    ["Remote"] = nil
+}
+
+local function buyMelee()
 
     if player:GetAttribute("ExactLocation") == "Submerged Island" then
         tween(CFrame.new(11426.201171875, -2155.0634765625, 9730.7080078125))
         game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Net"):WaitForChild("RF/SubmarineTransportation"):InvokeServer("InitiateTeleport", "Tiki Outpost")
         task.wait(2)
     else
-        tween(CFrame.new(-16516.1328125, 23.38727569580078, -189.69615173339844))
-        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("BuySanguineArt")
+        if not getgenv().meleeConfigs["Position"] or not getgenv().meleeConfigs["Remote"] then warn("return") return end
+        tween(getgenv().meleeConfigs["Position"])
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(getgenv().meleeConfigs["Remote"])
     end
 
 end
 
+local function setupSharkSet()
+
+    if player:GetAttribute("ExactLocation") == "Submerged Island" then
+        tween(CFrame.new(11426.201171875, -2155.0634765625, 9730.7080078125))
+        game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Net"):WaitForChild("RF/SubmarineTransportation"):InvokeServer("InitiateTeleport", "Tiki Outpost")
+        task.wait(2)
+    end
+
+    for _, name in game:GetDescendants() do
+        if name:IsA("Tool") and name.ToolTip == "Melee" and name.Name ~= "Sharkman Karate" and (name.Parent == player.Backpack or name.Parent == player.Character) then
+            tween(CFrame.new(-4972.06689453125, 314.4974365234375, -3218.112060546875))
+            task.wait(1)
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("BuySharkmanKarate")
+            break
+        end
+    end
+    
+    local itemList = {
+        {name = "Saber", id = 16}
+        -- {name = "Hallow Scythe", id = 160},
+        -- {name = "Cursed Dual Katana", id = 175},
+        -- {name = "Yama", id = 135},
+        -- {name = "Tushita", id = 136},
+        -- {name = "Spikey Trident", id = 168}
+    }
+    
+    local InventoryController = require(game:GetService("ReplicatedStorage").Controllers.UI.Inventory)
+    local ItemReplication = require(game:GetService("ReplicatedStorage").Util.ItemReplication)
+    
+    local foundItem = nil
+    
+    -- ตรวจสอบ Item ในกระเป๋า
+    for _, item in ipairs(InventoryController:GetTiles()) do
+        local itemId = item.ItemId
+        local uid = item.NetworkedUID
+        
+        -- เช็คว่า Item ID ตรงกับในตารางหรือไม่
+        for _, listItem in ipairs(itemList) do
+            if itemId == listItem.id then
+                if not foundItem then
+                    foundItem = listItem.name
+                end
+            end
+        end
+    end
+    
+    -- สวมใส่ Item ที่เจอตัวแรก
+    if foundItem then
+        local args = {
+            "LoadItem",
+            foundItem
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(args))
+    else
+        print("not found any any item")
+    end
+end
+
 spawn(function()
-    while task.wait() do
-        if getgenv().Autobuy then
-            main()
+    while task.wait(0.5) do
+        if getgenv().BuyMelee then
+            buyMelee()
+        end
+
+        if getgenv().Setup then
+            setupSharkSet()
         end
     end
 end)
@@ -101,7 +172,7 @@ end)
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local Window = WindUI:CreateWindow({
-    Title = "BuySanguineArt",
+    Title = "Setup item",
     Icon = "hand-fist", -- lucide icon. optional
     Author = "by: odyssey_0", -- optional
 })
@@ -130,12 +201,44 @@ Window:EditOpenButton({
 
 local Toggle = Tab:Toggle({
     Title = "Buy Melee",
-    Desc = "Buy item (SanguineArt)",
+    Desc = "Buy selected melee",
     Icon = "status",
     Type = "Checkbox",
-    Value = true, -- default value
-    Callback = function(state) 
+    Value = getgenv().Configs["Melee"]["Enable"], -- default value
+    Callback = function(state)
+        getgenv().BuyMelee = state
         if not state then getgenv().startTween:Cancel() end
-        getgenv().Autobuy = state
+    end
+})
+
+local Dropdown = Tab:Dropdown({
+    Title = "Select Melee",
+    Desc = "none",
+    Values = { "Sanguine Art", "Dragon Talon", "Sharkman Karate" },
+    Value = getgenv().Configs["Melee"]["Select"],
+    Callback = function(option)
+        if option == "Sanguine Art" then
+            print(option)
+            getgenv().meleeConfigs["Position"] = CFrame.new(-16516.1328125, 23.38727569580078, -189.69615173339844)
+            getgenv().meleeConfigs["Remote"] = "BuySanguineArt"
+        elseif option == "Dragon Talon" then
+            getgenv().meleeConfigs["Position"] = CFrame.new(5661.89794921875, 1210.876953125, 863.176025390625)
+            getgenv().meleeConfigs["Remote"] = "BuyDragonTalon"
+        elseif option == "Sharkman Karate" then
+            getgenv().meleeConfigs["Position"] = CFrame.new(-4971.2060546875, 313.8869323730469, -3223.0791015625)
+            getgenv().meleeConfigs["Remote"] = "BuySharkmanKarate"
+        end
+    end
+})
+
+local SetupShark = Tab:Toggle({
+    Title = "Setup",
+    Desc = "Saber && Sharkman Melee",
+    Icon = "status",
+    Type = "Checkbox",
+    Value = getgenv().Configs["Setup"]["Enable"], -- default value
+    Callback = function(state)
+        getgenv().Setup = state
+        if not state then getgenv().startTween:Cancel() end
     end
 })
